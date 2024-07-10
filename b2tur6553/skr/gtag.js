7a45e955d6d62f40e2c18752c4e704be9f30b9b2
atom213717 <86960272+atom213717@users.noreply.github.com>
2024-07-10 16:13:14 +0200

    a

--- b2tur6553/skr/gtag.js
+++
@@ -1,9 +0,0 @@
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=GT-PJ5VZNTG"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'GT-PJ5VZNTG');
-</script>
